15dcd7ef9e117ee67f4787c52cb08bd8f46d4452
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-12-27 16:41:09 -0700

    update and disable catflix, still broken

--- src/providers/sources/catflix.ts
+++ src/providers/sources/catflix.ts
@@ -1,49 +1,19 @@
+/* eslint-disable no-console */
 import { load } from 'cheerio';
 
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
-import { compareMedia } from '@/utils/compare';
-import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
-import { NotFoundError } from '@/utils/errors';
+import { MovieScrapeContext } from '@/utils/context';
 
 const baseUrl = 'https://catflix.su';
 
-async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
-  const searchPage = await ctx.proxiedFetcher('/', {
-    baseUrl,
-    query: {
-      s: ctx.media.title,
-    },
-  });
+async function comboScraper(ctx: MovieScrapeContext): Promise<SourcererOutput> {
+  const movieId = ctx.media.tmdbId;
+  const movieTitle = ctx.media.title.replace(/ /g, '-').toLowerCase();
 
-  ctx.progress(40);
-
-  const $search = load(searchPage);
-  const searchResults: { title: string; year?: number | undefined; url: string }[] = [];
-
-  $search('li').each((_, element) => {
-    const title = $search(element).find('h2').first().text().trim();
-    // the year is always present, but I sitll decided to make it nullable since the impl isn't as future-proof
-    const year = Number($search(element).find('.text-xs > span').eq(1).text().trim()) || undefined;
-    const url = $search(element).find('a').attr('href');
-
-    if (!title || !url) return;
-
-    searchResults.push({ title, year, url });
-  });
-
-  let watchPageUrl = searchResults.find((x) => x && compareMedia(ctx.media, x.title, x.year))?.url;
-  if (!watchPageUrl) throw new NotFoundError('No watchable item found');
+  const watchPageUrl = `${baseUrl}/movie/${movieTitle}-${movieId}`;
+  console.log('Watch page URL:', watchPageUrl);
 
   ctx.progress(60);
-
-  if (ctx.media.type === 'show') {
-    const match = watchPageUrl.match(/\/series\/([^/]+)\/?/);
-    if (!match) throw new Error('Failed to parse watch page url');
-    watchPageUrl = watchPageUrl.replace(
-      `/series/${match[1]}`,
-      `/episode/${match[1]}-${ctx.media.season.number}x${ctx.media.episode.number}`,
-    );
-  }
 
   const watchPage = load(await ctx.proxiedFetcher(watchPageUrl));
 
@@ -69,6 +39,6 @@ export const catflixScraper = makeSourcerer({
   name: 'Catflix',
   rank: 122,
   flags: [],
+  disabled: true,
   scrapeMovie: comboScraper,
-  scrapeShow: comboScraper,
 });
