511dbf50793d2ee32584f34c4475d72e415b9c69
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2025-01-01 16:45:39 -0700

    reorder autoembed

--- src/providers/sources/autoembed.ts
+++ src/providers/sources/autoembed.ts
@@ -38,7 +38,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 export const autoembedScraper = makeSourcerer({
   id: 'autoembed',
   name: 'Autoembed',
-  rank: 70,
+  rank: 92,
   disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
