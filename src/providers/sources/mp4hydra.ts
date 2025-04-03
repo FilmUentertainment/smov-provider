da41be79e3b3e8e46e1dc8066d815b29d31248e9
FilmUentertainment <filmu08@gmail.com>
2025-04-03 12:45:10 +0530

    Update mp4hydra.ts

--- src/providers/sources/mp4hydra.ts
+++ src/providers/sources/mp4hydra.ts
@@ -69,7 +69,7 @@ async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promis
 
 export const mp4hydraScraper = makeSourcerer({
   id: 'mp4hydra',
-  name: 'Mp4Hydra',
+  name: 'FilmU server 5',
   rank: 3,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
